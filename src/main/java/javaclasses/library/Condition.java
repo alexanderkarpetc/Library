1eb121cb2f7e9405a9cff66b2d53fab4932ec4e6
Dmytry Dyachenko <dmytry.dyachenko@teamdev.local>
2018-01-29 13:21:57 +0200

    Added Status class for books.

---
+++ src/main/java/javaclasses/library/Condition.java
@@ -0,0 +1,8 @@
+package javaclasses.library;
+
+public enum Condition {
+    IN_LIBRATY,
+    BORROWED,
+    ARRIVE_SOON,
+    AVAILABLE
+}
--- src/main/java/javaclasses/library/Status.java
+++ src/main/java/javaclasses/library/Status.java
@@ -1,4 +1,25 @@
 package javaclasses.library;
 
+import javaclasses.library.tinytype.UserId;
+
 public class Status {
+    private UserId userId;
+    private final Condition condition;
+
+    public Status(Condition condition) {
+        this.condition = condition;
+    }
+
+    public Status(UserId userId, Condition condition) {
+        this.userId = userId;
+        this.condition = condition;
+    }
+
+    public UserId getUserId() {
+        return userId;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
 }
