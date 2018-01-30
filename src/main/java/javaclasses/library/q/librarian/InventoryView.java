0335a332a2956dd6b1db708041a3e87fad28494e
Paul <paulageyev@gmail.com>
2018-01-30 16:10:54 +0200

    Refactored Inventory View.

--- src/main/java/javaclasses/library/q/librarian/InventoryView.java
+++ src/main/java/javaclasses/library/q/librarian/InventoryView.java
@@ -3,7 +3,7 @@ package javaclasses.library.q.librarian;
 import javaclasses.library.q.librarian.valueobjects.InventoryVO;
 import javaclasses.library.q.LoanStatus;
 
-public class InventoryPage {
+public class InventoryView {
 
     private InventoryVO inventoryVO;
     private LoanStatus status;
--- src/main/java/javaclasses/library/q/librarian/InventoryViewExtended.java
+++ src/main/java/javaclasses/library/q/librarian/InventoryViewExtended.java
@@ -2,7 +2,7 @@ package javaclasses.library.q.librarian;
 
 import javaclasses.library.q.librarian.valueobjects.InventoryVOExtended;
 
-public class InventoryPageExtended {
+public class InventoryViewExtended {
 
     private InventoryVOExtended inventoryVOExtended;
 }
