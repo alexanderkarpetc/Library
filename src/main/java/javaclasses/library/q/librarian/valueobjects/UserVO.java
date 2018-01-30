4cf04b3f5584ac871698f4d4447d937e6ea16051
alexander.karpetc <alexander.karpetc@gmail.com>
2018-01-30 17:04:07 +0200

    Add Readers projection, UserVO.

---
+++ src/main/java/javaclasses/library/q/librarian/valueobjects/UserVO.java
@@ -0,0 +1,11 @@
+package javaclasses.library.q.librarian.valueobjects;
+
+import javaclasses.library.c.UserId;
+
+public class UserVO {
+    private UserId userId;
+    private String userName;
+    private int currentlyReading;
+    private int reservations;
+    private int overdue;
+}
--- src/main/java/javaclasses/library/q/reader/BookView.java
+++ src/main/java/javaclasses/library/q/reader/BookView.java
@@ -2,12 +2,10 @@ package javaclasses.library.q.reader;
 
 import javaclasses.library.c.book.Author;
 import javaclasses.library.c.book.BookTitle;
-//Todo maybe bookview
 public class BookView {
     private byte[] image;
     private BookTitle title;
     private Author author;
-    //Todo: bookStatus
     private BookStatus status;
 
 }
