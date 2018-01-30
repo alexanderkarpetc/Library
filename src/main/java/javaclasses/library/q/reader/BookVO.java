59f28983d4d8b7802d52195b55adaef8ede0641e
alexander.karpetc <alexander.karpetc@gmail.com>
2018-01-30 14:36:20 +0200

    Create home page, BookVO for projection.

---
+++ src/main/java/javaclasses/library/q/reader/BookVO.java
@@ -0,0 +1,13 @@
+package javaclasses.library.q.reader;
+
+import javaclasses.library.c.book.Author;
+import javaclasses.library.c.book.BookTitle;
+
+public class BookVO {
+    private byte[] image;
+    private BookTitle title;
+    private Author author;
+    //Todo: bookStatus
+    private Status status;
+
+}
