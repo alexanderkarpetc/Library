aeb2592ac772d753ec0ed9ee12b577aa8c9a38ba
alexander.karpetc <alexander.karpetc@gmail.com>
2018-01-30 13:33:24 +0200

    Created homepage and details view.

---
+++ src/main/java/javaclasses/library/q/user/BookDetails.java
@@ -0,0 +1,13 @@
+package javaclasses.library.q.user;
+
+import javaclasses.library.c.book.BookDescription;
+import javaclasses.library.c.book.BookVO;
+
+/**
+ * Pop-up after click the book.
+ */
+public class BookDetails {
+    private BookVO bookVO;
+    private BookDescription description;
+
+}
---
+++ src/main/java/javaclasses/library/q/user/HomePage.java
@@ -0,0 +1,24 @@
+package javaclasses.library.q.user;
+
+import javaclasses.library.c.Status;
+import javaclasses.library.c.UserId;
+import javaclasses.library.c.book.BookVO;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Represents home page. Books are sorted according to their status:
+ * <li>
+ *     <ul>first overdue</ul>
+ *     <ul>second borrowed</ul>
+ *     <ul>third reserved</ul>
+ * </li>
+ * (or maybe some more sorting: for example for new hot books)
+ */
+public class HomePage {
+    private Map<BookVO, Status> books = new TreeMap<>();
+    private Collection<Category> categories; //???
+    private UserId currentUser;//???
+
+}
