3d073025af218c591cf8fb3ebd43c85a1f5c6eba
alexander.karpetc <alexander.karpetc@gmail.com>
2018-01-31 12:42:01 +0200

    Add LoanVO.

--- src/main/java/javaclasses/library/q/librarian/BookCompactHistory.java
+++ src/main/java/javaclasses/library/q/librarian/BookCompactHistory.java
@@ -1,6 +1,10 @@
 package javaclasses.library.q.librarian;
 
 
+import javaclasses.library.q.librarian.valueobjects.LoanVO;
+
+import java.util.List;
+
 public class BookCompactHistory {
     //Todo[Karpers]:create loanVO
     List<LoanVO> loans;
