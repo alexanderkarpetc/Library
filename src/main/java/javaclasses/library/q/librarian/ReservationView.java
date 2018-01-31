bf397606fecfef763c8b5d8d2bcd8a23021864d7
Paul <paulageyev@gmail.com>
2018-01-31 12:55:25 +0200

    Added ReservationVO. Refactored Reservation View.

--- src/main/java/javaclasses/library/q/librarian/ReservationView.java
+++ src/main/java/javaclasses/library/q/librarian/ReservationView.java
@@ -3,13 +3,15 @@ package javaclasses.library.q.librarian;
 import javaclasses.library.c.book.Author;
 import javaclasses.library.c.book.BookTitle;
 import javaclasses.library.c.book.Image;
-import sun.rmi.server.InactiveGroupException;
-
-import java.util.Date;
-import java.util.List;
+import javaclasses.library.q.librarian.valueobjects.ReservationVO;
 
 public class ReservationView {
+
     private final Image image;
-    private final ReservationVO;
-    //Todo [paul]
+    private final ReservationVO reservation;
+
+    public ReservationView(Image image, ReservationVO reservation) {
+        this.image = image;
+        this.reservation = reservation;
+    }
 }
---
+++ src/main/java/javaclasses/library/q/librarian/valueobjects/ReservationVO.java
@@ -0,0 +1,24 @@
+package javaclasses.library.q.librarian.valueobjects;
+
+import javaclasses.library.c.UserId;
+import javaclasses.library.c.book.Author;
+import javaclasses.library.c.book.BookTitle;
+
+import java.util.Date;
+import java.util.List;
+
+public class ReservationVO {
+
+    private final BookTitle title;
+    private final List<Author> authors;
+    private final UserId userId;
+    private final Date madeON;
+
+    public ReservationVO(BookTitle title, List<Author> authors, UserId userId, Date madeON) {
+        this.title = title;
+        this.authors = authors;
+        this.userId = userId;
+        this.madeON = madeON;
+    }
+
+}
