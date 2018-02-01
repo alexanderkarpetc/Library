c8d9f3684f19dc5d38eddfa296d63999bbd3537c
alexander.karpetc <alexander.karpetc@gmail.com>
2018-02-01 14:18:02 +0200

    Add docs for ids

--- src/main/java/javaclasses/library/c/inventory/InventoryId.java
+++ src/main/java/javaclasses/library/c/inventory/InventoryId.java
@@ -3,7 +3,9 @@ package javaclasses.library.c.inventory;
 import javaclasses.library.c.book.BookId;
 
 public class InventoryId {
-
+    /**
+     * The same as the bookId.
+     */
     private final BookId bookId;
 
     public InventoryId(BookId bookId) {
--- src/main/java/javaclasses/library/c/inventory/RFID.java
+++ src/main/java/javaclasses/library/c/inventory/RFID.java
@@ -1,6 +1,9 @@
 package javaclasses.library.c.inventory;
 
 public class RFID {
+    /**
+     * Radio-frequency identification of the magnet.
+     */
     private final String rfid;
 
     public RFID(String rfid) {
