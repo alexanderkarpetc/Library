4979eeaa265e7ceb73aac74368da725ffbd410f9
alexander.karpetc <alexander.karpetc@gmail.com>
2018-02-02 13:16:43 +0200

    Add HomePage, Dialog Window

---
+++ src/main/java/javaclasses/library/q/reader/projection/DialogWindow.java
@@ -0,0 +1,12 @@
+package javaclasses.library.q.reader.projection;
+
+import javaclasses.library.q.reader.BookView;
+
+public class DialogWindow {
+
+    private final BookView book;
+
+    public DialogWindow(BookView book) {
+        this.book = book;
+    }
+}
