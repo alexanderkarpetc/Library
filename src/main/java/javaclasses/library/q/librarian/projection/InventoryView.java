cb54b89ce0902f874e265ec8c1194afa47272602
Dmytry-Dyachenko <33631721+Dmytry-Dyachenko@users.noreply.github.com>
2018-02-02 16:01:27 +0200

    Add JavaDoc to `InventoryView`

--- src/main/java/javaclasses/library/q/librarian/projection/InventoryView.java
+++ src/main/java/javaclasses/library/q/librarian/projection/InventoryView.java
@@ -4,7 +4,14 @@ import javaclasses.library.q.librarian.InventoryDetails;
 
 import java.util.List;
 
-//Todo [dima]
+/**
+ * {@code InventoryView} there are first view that appears for librarian.
+ *
+ * <br><p>This view presents like a table with all books that library has.
+ * <br>In the highest part of table the books that have overdue items.
+ * <br>On this view librarian can remove book, edit it, or move to book details.
+ *
+ */
 public class InventoryView {
 
     private final List<InventoryDetails> books;
